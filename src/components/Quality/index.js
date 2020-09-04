7dc6e9aee36647769852db4a06eadc6f726bb97e
Juan Daniel Sanchez <32243398+DseeHappy@users.noreply.github.com>
2020-09-03 20:19:56 -0600

    resize shite

--- src/components/Quality/index.js
+++ src/components/Quality/index.js
@@ -39,50 +39,48 @@ const Quality = () => {
             </div>
 
             <div className="col-12 col-md-6">
-            <div className={style.center__content}>
-
-              <h3 className="text-center mb-4">
-                Quality Licensed Professionals
-              </h3>
-              <p className="op__descr text-center text-muted mb-5">
-                Our moving teams are trained and always ready to help you with
-                your move. We understand when you select a moving company its
-                more than just stuff we're moving. It's what makes your house a
-                home and we want to respect that on every level. From the
-                loading stacked to the tip top of the truck to the delivery to
-                your new home. Our professional are always making sure their
-                first priority is Quality Service.
-              </p>
-              <div className="row">
-                <div className=" col-sm-5 col-md-5 col-12">
+              <div className={style.center__content}>
+                <h3 className="text-center mb-4">
+                  Quality Licensed Professionals
+                </h3>
+                <p className="op__descr text-center text-muted mb-5">
+                  Our moving teams are trained and always ready to help you with
+                  your move. We understand when you select a moving company its
+                  more than just stuff we're moving. It's what makes your house
+                  a home and we want to respect that on every level. From the
+                  loading stacked to the tip top of the truck to the delivery to
+                  your new home. Our professional are always making sure their
+                  first priority is Quality Service.
+                </p>
+                <div className="row">
+                  <div className=" col-sm-5 col-md-5 col-12">
+                    <div
+                      className={style.rec__iteminsured}
+                      data-aos="fade-up"
+                      data-aos-delay="600"
+                    >
+                      <Img fluid={data.Insured.childImageSharp.fluid} />
+                    </div>
+                  </div>
+                  <div className=" col-sm-7 col-md-7 col-12">
+                    <div
+                      className={style.rec__itemquote}
+                      data-aos="fade-up"
+                      data-aos-delay="600"
+                    >
+                      <Img fluid={data.Quote.childImageSharp.fluid} />
+                    </div>
+                  </div>
                   <div
-                    className={style.rec__iteminsured}
+                    className={style.center__callbtn}
                     data-aos="fade-up"
                     data-aos-delay="600"
                   >
-                    <Img fluid={data.Insured.childImageSharp.fluid} />
+                    <a href="tel:7208297961" className="op__btn mr-3">
+                      Call Now
+                    </a>
                   </div>
                 </div>
-                <div className=" col-sm-7 col-md-7 col-12">
-                  <div
-                    className={style.rec__itemquote}
-                    data-aos="fade-up"
-                    data-aos-delay="600"
-                  >
-                    <Img fluid={data.Quote.childImageSharp.fluid} />
-                  </div>
-                </div>
-                <div
-                  className=" col-12"
-                  data-aos="fade-up"
-                  data-aos-delay="600"
-                >
-                  <a href="tel:7208297961" className="op__btn mr-3">
-                    Call Now
-                  </a>
-                </div>
-                </div>
-
               </div>
             </div>
           </div>
